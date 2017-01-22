8ef607e0eb01276ad4dba8497575e07e9c10e3fd
alanrkessler <alan.r.kessler@gmail.com>
2017-01-22 09:04:13 -0600

    Updated for 2017
    This was my first Shiny app and one of my first experiences with R.
    Updating the app to clean it up and update data for 2017. Added auto
    complete text for player names.

--- global.R
+++ global.R
@@ -1,128 +1,139 @@
 library(shiny)
+library(shinysky)
 library(DT)
 library(dplyr)
 library(ggvis)
+library(stringr)
 
-#Number of teams in the league
-tms<-20
+# Number of teams in the league
+tms <- 18
 
-#Import batters (b) and pitchers (p)
-b<-read.csv("./data/batters.csv")
-p<-read.csv("./data/pitchers.csv")
+# Import batters and pitchers
+batters <- read.csv("./data/batters.csv")
+pitchers <- read.csv("./data/pitchers.csv")
 
-#Select variables
-b<-b %>% select(PlayerName,Team,POS,PA,Dollars)
-p<-p %>% select(PlayerName,Team,POS,IP,Dollars)
+# Select variables using a standard naming and switch first and last names
+b <- batters %>% 
+  select(PlayerName, Team, POS, PA.IP = PA, Dollars) %>% 
+  mutate(PlayerName = paste0(word(PlayerName, -1), 
+                             ", ", word(PlayerName, 1, -2)))
 
-#Create a standard variable for innings and plate appearaces
-colnames(b)[4]<-"PA.IP"
-colnames(p)[4]<-"PA.IP"
+p <- pitchers %>% 
+  select(PlayerName, Team, POS, PA.IP = IP, Dollars) %>% 
+  mutate(PlayerName = paste0(word(PlayerName, -1),
+                             ", ", word(PlayerName, 1, -2)))
+# Format Dollars 
+all.players <- rbind(b, p) %>%  # Combine batters and pitchers data
+  mutate(Dollars = as.numeric(gsub(" ", "", chartr('$)(', '  -', Dollars)))) %>%
+  arrange(desc(Dollars)) %>%  # Sort by dollars descending
+  dplyr::mutate(Ovr.Rank = 1:n())  # Create ranking id
 
-#Merge b & p
-all_players<-rbind(b,p)
-rm(b,p)
-
-#Convert dollars to numeric
-#Remove dollar signs
-all_players$Dollars<-(sub('$','',as.character(all_players$Dollars),fixed=TRUE))
-#Remove negatives
-all_players$Dollars<-(sub(')','',as.character(all_players$Dollars),fixed=TRUE))
-#Convert to numeric
-all_players$Dollars<-as.numeric(sub('(','-',as.character(all_players$Dollars),fixed=TRUE))
-
-#Sort by dollars descending
-all_players<-all_players[order(-all_players$Dollars),]
-
-#Reset the row names
-rownames(all_players) <- seq(length=nrow(all_players))
-
-#Provide an ID to each value
-all_players$Ovr.Rank<-1:nrow(all_players)
-
-#Function that categorizes players into 1 of 5 possible tiers for each position
-#Tier 5 are those with fewer PA/IP than the set threshold
-#Tier 4 are those with a rank worse than the number of starting spots on all teams
-#Tiers 1-3 are set based on k-means clustering using the dollar amount 
-#Select a position, spots for that position in the league, and minimum PA/IP
-player_tier<-function(pos="C",spts=1,flr=200){
-     #Copy data
-     fil<-all_players
-     #Filter for the selected position
-     fil<-fil[grepl(pos,fil$POS)==TRUE,]
-     #Rank by dollars
-     fil$rank<-rank(-fil$Dollars)
-     
-     #If less than the min PA.IP go to tier 5, 
-     #If worse than number of spots go to tier 4,
-     #Otherwise make temporary tier 1
-     fil$Tier<-ifelse(fil$PA.IP<flr,5,ifelse(fil$rank>(tms*spts),4,1))
-     
-     #Create separate data frames for top tiers and bottom tiers
-     fil1<-fil[fil$Tier==1,]
-     fil2<-fil[fil$Tier!=1,]
-     
-     #Cluster top tiers
-     filk<-kmeans(fil1$Dollars,3,iter.max=1000,nstart=10)
-     
-     #Create a table of assignments to clusters
-     filk_assign<-data.frame(filk$cluster,1:length(filk$cluster))
-     names(filk_assign)<-c("c","order")
-     
-     #Create custom cluster number scheme
-     filk_clusname<-data.frame(1:3,rank(-filk$centers))
-     names(filk_clusname)<-c("c","cluster")
-     
-     #Merge assignments and custom names and order
-     fil_merge<-merge(filk_assign,filk_clusname)
-     fil_merge<-fil_merge[order(fil_merge$order),3]
-     
-     #Bind tier assignments
-     fil1<-cbind(fil1,fil_merge)
-     
-     #Overwrite tier with cluster
-     fil1$Tier<-fil1$fil_merge
-     
-     #Remove temporary variables
-     fil1$fil_merge<-NULL
-     fil1$rank<-NULL
-     fil2$rank<-NULL
-     
-     #Recombine top and bottom tier groups
-     fil_fin<-rbind(fil1,fil2)
-     
-     #Rename tier to show position
-     colnames(fil_fin)[7]<-paste("Tier",pos,sep=".")
-     
-     #Return tiers for the selected position
-     fil_fin
+player.tier <- function(pos = "C", spts = 1, flr = 200) {
+  # Assigns tiers to each player at each position 1-5 (best - worst)
+  #
+  # Args:
+  #   pos: Position.
+  #   spts: Starting spots on the fantasy roster
+  #   flr: The minimum number of PA or IP required to be above tier 5
+  #
+  # Returns:
+  #   The dataframe for that position with a new column corresponding to tier.
+  fil <- all.players %>%  # Copy data
+   filter(grepl(pos, POS) == TRUE) %>%  # Filter for the position
+   mutate(rank = rank(-Dollars),  # Create position rank
+          Tier = ifelse(PA.IP < flr, 5,  # Create initial tiers
+                        ifelse( rank > (tms * spts), 4, 1)))
+  
+  # Create separate data frames for top tiers and bottom tiers
+  fil.top <- fil[fil$Tier == 1, ]
+  fil.bottom <- fil[fil$Tier != 1, ]
+  
+  # Cluster for tiers 1 - 3 
+  clusters <- kmeans(fil.top$Dollar, 3, iter.max = 1000, nstart = 10)
+  
+  # Create a table of assignments to clusters
+  cluster.assignments <- data.frame(clusters$cluster, 
+                                   1:length(clusters$cluster))
+  names(cluster.assignments) <- c("c", "order")
+  
+  # Have clusters named in numerical order
+  cluster.names <- data.frame(1:3, rank(-clusters$centers))
+  names(cluster.names) <- c("c", "cluster")
+  
+  # Merge assignments and custom names and order
+  tiers.top <- inner_join(cluster.assignments, cluster.names, by = "c") %>%
+   select(cluster)
+  
+  # Overwrite tier with cluster
+  fil.top$Tier <- tiers.top$cluster
+  
+  #Recombine top and bottom tier groups
+  fil.final <- rbind(fil.top, fil.bottom) %>%
+   select(-rank)  # Drop rank column
+  
+  # Rename tier based on position
+  colnames(fil.final)[7] <- paste("Tier", pos, sep = ".")
+  
+  # Return tiers for the selected position
+  fil.final
 }
 
-#Create a copy before joining tier assignments
-a_start<-all_players
-#Join tier assignments
-a_start<-merge(a_start,player_tier("C",1,200),all=TRUE)
-a_start<-merge(a_start,player_tier("1B",1,200),all=TRUE)
-a_start<-merge(a_start,player_tier("2B",1,200),all=TRUE)
-a_start<-merge(a_start,player_tier("3B",1,200),all=TRUE)
-a_start<-merge(a_start,player_tier("SS",1,200),all=TRUE)
-a_start<-merge(a_start,player_tier("OF",3,200),all=TRUE)
-a_start<-merge(a_start,player_tier("SP",4,50),all=TRUE)
-a_start<-merge(a_start,player_tier("RP",1,20),all=TRUE)
-a_start<-merge(a_start,player_tier("P",2,20),all=TRUE)
+# Join tier assignments and order by dollars
+tier.data <- full_join(player.tier("C", 1, 200),
+                       player.tier("1B", 1, 200),
+                       by = c("PlayerName", "Team", "POS", 
+                              "PA.IP", "Dollars", "Ovr.Rank")) %>%
+  full_join(., player.tier("2B", 1, 200), by = c("PlayerName", "Team", "POS", 
+                                                 "PA.IP", "Dollars", 
+                                                 "Ovr.Rank")) %>%
+  full_join(., player.tier("3B", 1, 200), by = c("PlayerName", "Team", "POS", 
+                                                 "PA.IP", "Dollars", 
+                                                 "Ovr.Rank")) %>%
+  full_join(., player.tier("SS", 1, 200), by = c("PlayerName", "Team", "POS", 
+                                                 "PA.IP", "Dollars", 
+                                                 "Ovr.Rank")) %>%
+  full_join(., player.tier("OF", 3, 200), by = c("PlayerName", "Team", "POS", 
+                                                 "PA.IP", "Dollars", 
+                                                 "Ovr.Rank")) %>%
+  full_join(., player.tier("SP", 4, 50), by = c("PlayerName", "Team", "POS", 
+                                                 "PA.IP", "Dollars", 
+                                                 "Ovr.Rank")) %>%
+  full_join(., player.tier("RP", 1, 20), by = c("PlayerName", "Team", "POS", 
+                                                 "PA.IP", "Dollars", 
+                                                 "Ovr.Rank")) %>%
+  full_join(., player.tier("P", 2, 20), by = c("PlayerName", "Team", "POS", 
+                                                 "PA.IP", "Dollars", 
+                                                 "Ovr.Rank")) %>%
+  arrange(desc(Dollars))
 
-#Sort by Dollars
-tier_data<-a_start[order(-a_start$Dollars),]
-rm(a_start)
+# Calculate max tier
+tier.data$Tier <- do.call(pmin, c(tier.data[7:15], na.rm=T))
 
-#Create lists for selectInputs
-#Positions including "All" (for draft sheet filter)
-positions_all<-c("All","C","1B","2B","3B","SS","OF","SP","RP","P")
-#Positions exlcuding "All" (for plot filter)
-positions<-c("C","1B","2B","3B","SS","OF","SP","RP","P")
-#X-axis options (for plot)
-xaxis<-c("Pos.Rank","Ovr.Rank")
-names(xaxis)<-c("Position Rank","Overall Rank")
+# Create lists for selectInputs
+# Positions including "All" (for draft sheet filter)
+positions.all <- c("All", "C", "1B", "2B", "3B", "SS", "OF", "SP", "RP", "P")
+# Positions exlcuding "All" (for plot filter)
+positions <- positions.all[-1]
+# X-axis options (for plot)
+xaxis <- c("Pos.Rank", "Ovr.Rank")
+names(xaxis) <- c("Position Rank", "Overall Rank")
 
-#Generate tier table before the draft
-tier_start<-cbind(1:5,as.data.frame(lapply(tier_data[,7:15],table))[,seq(2, ncol(tier_data[,7:15])*2, by = 2)])
-names(tier_start)<-c("Tier","Pos.C","Pos.1B","Pos.2B","Pos.3B","Pos.SS","Pos.OF","Pos.SP","Pos.RP","Pos.P")
+# Generate tier table before the draft
+tier.start <- cbind(1:5, as.data.frame(lapply(tier.data[, 7:15], table))[, seq(2, ncol(tier.data[, 7:15]) * 2, by = 2)])
+names(tier.start) <- c("Tier", "Pos.C", "Pos.1B", "Pos.2B", "Pos.3B", "Pos.SS",
+                       "Pos.OF", "Pos.SP", "Pos.RP", "Pos.P")
+
+# Create autocomplete list
+autocomplete.list <- all.players %>% select(PlayerName)
+
+# Merge on max tier
+all.players <- tier.data %>% 
+  select(PlayerName, Tier) %>%
+  left_join(., all.players, by = "PlayerName") %>%
+  select(PlayerName, Team, POS, Tier, PA.IP, Dollars, Ovr.Rank)
+
+# List for team drop down
+teams.all <- c("All", as.character(sort(unique(all.players$Team))))
+
+# Clean up environment
+rm(b, p, batters, pitchers)
--- ui.R
+++ ui.R
@@ -2,28 +2,37 @@ shinyUI(navbarPage("Fantasy Baseball Draft App",
      tabPanel("Draft Board",
           fluidPage(
                fluidRow(align="left",
-                    #Selection list for position
-                    column(4,selectInput('posa', 'Position', positions_all)),
-                    #Text input to remove players when they are drafted
-                    column(4,textInput("text", label = "Player", value = ""),
-                    #Delete button 
-                    actionButton(inputId = "delete.button", label = "Delete", icon = icon("minus")))
+                    # Selection list for position
+                    column(4, selectInput('posa', 'Position', positions.all)),
+                    # Selection list for position
+                    column(4, selectInput('teama', 'Team', teams.all)),
+                    # Text input to remove players when they are drafted
+                    column(4, strong('Player'), br(),
+                           textInput.typeahead(id = "text", 
+                                               placeholder = "", 
+                                               local = autocomplete.list,
+                                               valueKey = "PlayerName",
+                                               tokens = c(1:dim(autocomplete.list)[1]),
+                                               template="<p class='repo-language'>{{info}}</p> <p class='repo-name'>{{PlayerName}}</p>")),
+                    
+                    # Delete button 
+                    column(4, br(), actionButton(inputId = "delete.button", label = "Delete", icon = icon("minus")))
                ),
-               fluidRow(align="center",
-                    #Draft sheet
+               fluidRow(align = "center",
+                    # Draft sheet
                     DT::dataTableOutput("sheet")
                )
           )
      ),
      tabPanel("Player Tiers",
               fluidPage(
-                   fluidRow(align="center",
-                        #Display tiers after removing drafted players
+                   fluidRow(align = "center",
+                        # Display tiers after removing drafted players
                         h4("Current Tiers"),
                         DT::dataTableOutput("tierc")
                    ),
-                   fluidRow(align="center",
-                        #Display tiers at the start of the draft
+                   fluidRow(align = "center",
+                        # Display tiers at the start of the draft
                         h4("Starting Tiers"),
                         DT::dataTableOutput("tiers")
                    )
@@ -31,21 +40,19 @@ shinyUI(navbarPage("Fantasy Baseball Draft App",
      ),
      tabPanel("Plots",
               fluidPage(
-                   fluidRow(align="left",
-                        #Selection list for position
-                        column(4,selectInput('pos', 'Position', positions)),
-                        #Choose plotting by overall rank or position rank
-                        column(4,selectInput('xvar', 'Rank', xaxis)),
-                        column(4,selectInput('drft', 'Show Drafted', c("Yes","No")))
+                   fluidRow(align = "left",
+                        # Selection list for position
+                        column(4, selectInput('pos', 'Position', positions)),
+                        # Choose plotting by overall rank or position rank
+                        column(4, selectInput('xvar', 'Rank', xaxis)),
+                        column(4, selectInput('drft', 'Show Drafted', c("Yes", "No")))
                    ),
-                   fluidRow(align="center",
+                   fluidRow(align = "center",
                         h4("Talent by Position Rank"),
                         ggvisOutput("plot1")
                    ),
-                   fluidRow(align="center",
+                   fluidRow(align = "center",
                         uiOutput("ui")
-                            #sliderInput("range","",
-                                    #min = 1, max = 500, value = c(1,50),width=800)
                    )
               )
      ),
